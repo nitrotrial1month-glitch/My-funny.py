febc442185628a57d3f17399e8985d2f1c477b47
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-02-14 11:59:16 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "<h1>Funny Bot is Online!</h1>"
+
+def run():
+    # Render পোর্ট 8080 বা এনভায়রনমেন্ট পোর্ট ব্যবহার করে
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+  
