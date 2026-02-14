3f1f7353741e650570560b1a81ecdcb6834bfc4a
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-02-14 12:03:13 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from threading import Thread
 
@@ -6,13 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "<h1>Funny Bot is Online!</h1>"
+    return "<h1>Funny Bot is Alive!</h1>"
 
 def run():
-    # Render পোর্ট 8080 বা এনভায়রনমেন্ট পোর্ট ব্যবহার করে
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-  
