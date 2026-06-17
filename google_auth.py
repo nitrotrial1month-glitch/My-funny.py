b1f0686e35614f7d33d672c7a03bbce8ff3b74f3
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-06-17 11:53:55 +0530

    Add Google login blueprint and route

--- google_auth.py
+++ google_auth.py
@@ -1,1 +1,9 @@
 
+from flask import Blueprint
+
+google_bp = Blueprint('google', __name__)
+
+@google_bp.route('/login/google')
+def login_google():
+    return "🌐 Google Login API Setup will be done here soon."
+  
