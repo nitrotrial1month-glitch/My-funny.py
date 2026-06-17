7a50b8dd1dfe4405c2e27fca3a9c8a7df1d848f2
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-06-17 11:54:13 +0530

    Update facebook_auth.py

--- facebook_auth.py
+++ facebook_auth.py
@@ -1,1 +1,9 @@
 
+from flask import Blueprint
+
+facebook_bp = Blueprint('facebook', __name__)
+
+@facebook_bp.route('/login/facebook')
+def login_facebook():
+    return "📘 Facebook Login API Setup will be done here soon."
+  
