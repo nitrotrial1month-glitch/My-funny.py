2f2a5381af2b1b3564616102f7c582b23cfddad2
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-06-17 11:53:34 +0530

    Update apple_auth.py

--- apple_auth.py
+++ apple_auth.py
@@ -1,1 +1,9 @@
 
+from flask import Blueprint
+
+apple_bp = Blueprint('apple', __name__)
+
+@apple_bp.route('/login/apple')
+def login_apple():
+    return "🍏 Apple iOS Login API Setup will be done here soon."
+  
